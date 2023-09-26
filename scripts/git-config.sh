60b76d01a404edf15a77227b15d451d007608ae5
Emma O'Leary <74734235+emoleary@users.noreply.github.com>
2023-09-26 14:25:27 -0700

    Update git-config.sh

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Enable us to push, through email privacy features
-git config --global user.email 148553+cryptogoth@users.noreply.github.com
+git config --global user.email 74734235+emoleary@users.noreply.github.com
